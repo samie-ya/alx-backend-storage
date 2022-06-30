dba4751562d2b5508d73b72a5e4c2cb3820cc9bd
Samie <samrasolomon40@gmail.com>
2022-06-30 10:14:39 +0000

    Added a file

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+-- This script will create a stored procedure called ComputeAverageScoreForUser
+DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+NOT DETERMINISTIC
+BEGIN
+	DECLARE total INT;
+	DECLARE amount INT;
+	DECLARE ave FLOAT;
+	SELECT SUM(score) INTO total FROM corrections WHERE user_id = user_id;
+	SELECT COUNT(score) INTO amount FROM corrections WHERE user_id = user_id;
+	SET ave = total / amount;
+	UPDATE users SET average_score = ave WHERE id = user_id;
+END//
+DELIMITER ;//
