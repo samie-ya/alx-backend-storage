bb07865a80d7733746a172f13afda73927389afd
Samie <samrasolomon40@gmail.com>
2022-07-05 10:56:03 +0000

    Modified structural changes

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,10 +2,9 @@
 """This script will list all document in a collection"""
 
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """This function will return list of all document"""
-        lists = mongo_collection.find()
-        if (lists):
-            return lists
-        return []
+def list_all(mongo_collection):
+    """This function will return list of all document"""
+    lists = mongo_collection.find({})
+    if (lists):
+        return lists
+    return []
