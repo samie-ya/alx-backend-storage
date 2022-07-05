599e405902a11741aa7b19ef738850d668977906
Samie <samrasolomon40@gmail.com>
2022-07-05 12:02:37 +0000

    Added a file

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This script will deal with average score of students"""
+import pymongo
+
+
+def top_students(mongo_collection):
+    """This function will return the average scores of students"""
+    for student in mongo_collection.find():
+        total = 0
+        for subject in student["topics"]:
+            total += subject["score"]
+        ave = total / 3
+        mongo_collection.update_one(student,
+                                    {'$set': {"averageScore": ave}})
+    return mongo_collection.find().sort("averageScore", pymongo.DESCENDING)
