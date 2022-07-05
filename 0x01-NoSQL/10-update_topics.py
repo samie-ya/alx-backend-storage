e896838e8e1b073bd75874ffda3c9736c4f81f87
Samie <samrasolomon40@gmail.com>
2022-07-05 11:16:06 +0000

    Added a file

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script will deal with update in pymongo"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """This function will update an existing document"""
+    mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}})
