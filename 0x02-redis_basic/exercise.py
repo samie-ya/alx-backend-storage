90b8e80bbdc30bf01da9eb8f32f23fcb1c1d69c4
Samie <samrasolomon40@gmail.com>
2022-07-06 13:38:02 +0000

    Just trying to check

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """This script will create a class to run redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Any
 
 
 class Cache:
@@ -17,3 +17,9 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Any]):
+        """This function will convert a data back to its original type"""
+        if (callable(fn)):
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
