9280f2f6a79764d97da17edc0f180e6eea270ea5
Samie <samrasolomon40@gmail.com>
2022-07-06 12:35:13 +0000

    Made annontation for self

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,14 +5,14 @@ import uuid
 from typing import Union
 
 
-class Cache():
+class Cache:
     """This class will create a connection to redis"""
     def __init__(self):
         """This function will intialize a class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, int, float, bytes]) -> str:
+    def store(self: redis.Redis, data: Union[str, int, float, bytes]) -> str:
         """This function will take an argument and return a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
