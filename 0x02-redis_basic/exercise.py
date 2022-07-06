f0464f3a419e4ba011260cb139249076552777e2
Samie <samrasolomon40@gmail.com>
2022-07-06 12:27:25 +0000

    Made attribute private

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,11 +9,11 @@ class Cache():
     """This class will create a connection to redis"""
     def __init__(self):
         """This function will intialize a class"""
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, int, float, bytes]) -> str:
         """This function will take an argument and return a string"""
         key = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
         return key
